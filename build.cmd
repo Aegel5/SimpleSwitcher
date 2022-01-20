d4e3396467b419219af7bc0258cada34cf9d2f0c
alex <alexzhigay@mail.ru>
2022-01-20 22:37:58 +0300

    del release_sign

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-﻿
+﻿download last release wxwidgets
+
+
+$(SolutionDir)$(Configuration)\verinc.exe "$(ProjectDir)\ver.h"
+signtool.exe sign /fd SHA256 /v /f "$(SolutionDir)\cert\SimpleSwitcher.pfx" /p m27SPnDeDZgz9Fvw483N /ph /t http://timestamp.comodoca.com/authenticode "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" & $(SolutionDir)$(Configuration)\checksum.exe sha256 "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" toFile
