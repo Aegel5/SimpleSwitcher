9e2dd09174e2ffef77827f3480c14276260ac582
alex <alexzhigay@mail.ru>
2022-01-21 14:50:42 +0300

    1

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 ﻿download last release wxWidgets and unpack to folder wxWidgets
-build release and debug with non shared runtime
+
 
 
 $(SolutionDir)$(Configuration)\verinc.exe "$(ProjectDir)\ver.h"
 signtool.exe sign /fd SHA256 /v /f "$(SolutionDir)\cert\SimpleSwitcher.pfx" /p m27SPnDeDZgz9Fvw483N /ph /t http://timestamp.comodoca.com/authenticode "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" & $(SolutionDir)$(Configuration)\checksum.exe sha256 "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" toFile
+
+add vissual studio runtimetime file
