8312c91a92bf6c7a841acaaa74d152647a3d86e5
alex <alexzhigay@mail.ru>
2022-01-23 19:53:31 +0300

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,8 +1,7 @@
-﻿download last release wxWidgets and unpack to folder wxWidgets
+﻿mkdir package_build
+cd package_build
+cmake -G "Visual Studio 17 2022" -A Win32 -DCMAKE_BUILD_TYPE=Release .
+cmake --build .
 
-
-
-$(SolutionDir)$(Configuration)\verinc.exe "$(ProjectDir)\ver.h"
-signtool.exe sign /fd SHA256 /v /f "$(SolutionDir)\cert\SimpleSwitcher.pfx" /p m27SPnDeDZgz9Fvw483N /ph /t http://timestamp.comodoca.com/authenticode "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" & $(SolutionDir)$(Configuration)\checksum.exe sha256 "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" toFile
-
-add vissual studio runtimetime file
+rem $(SolutionDir)$(Configuration)\verinc.exe "$(ProjectDir)\ver.h"
+rem signtool.exe sign /fd SHA256 /v /f "$(SolutionDir)\cert\SimpleSwitcher.pfx" /p m27SPnDeDZgz9Fvw483N /ph /t http://timestamp.comodoca.com/authenticode "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" & $(SolutionDir)$(Configuration)\checksum.exe sha256 "$(SolutionDir)$(Configuration)\$(TargetName)$(TargetExt)" toFile
