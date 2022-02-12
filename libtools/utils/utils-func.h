e3ce918a476adbf89940be8d133f83d227f3fc9b
alex <alexzhigay@mail.ru>
2022-02-12 22:45:24 +0300

    1

--- libtools/utils/utils-func.h
+++ libtools/utils/utils-func.h
@@ -19,7 +19,7 @@ namespace Utils
 	inline std::wstring GetNameForHKL_Unique(HKL hkl)
 	{
 		std::wstringstream stream;
-		stream << GetNameForHKL_simple(hkl) << " " << std::hex << TUInt64(hkl);
+		stream << GetNameForHKL_simple(hkl) << " (0x" << std::hex << TUInt64(hkl) << ")";
 		return stream.str();
 	}
 
