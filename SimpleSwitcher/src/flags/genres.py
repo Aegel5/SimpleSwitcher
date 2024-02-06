b3c14a31ca7da777242be07910cbaa0dc9daae0a
Alexsander <aegel5@yandex.ru>
2024-02-06 11:34:00 +0300

    flags stuff

---
+++ SimpleSwitcher/src/flags/genres.py
@@ -0,0 +1,20 @@
+﻿import pathlib
+from pathlib import Path
+import re
+import os
+import shutil 
+import sys
+
+resol = sys.argv[1]
+
+result = ''
+
+for root, dirs, files in os.walk('./png{}px'.format(resol)):
+    for file in files:
+        if file.endswith('.png'):    
+            result += '{}{} RCDATA "flags/png{}px/{}"\n'.format(
+                Path(file).stem, resol,resol, os.path.basename(file)
+            )
+            
+Path("./flag_res{}.rc".format(resol)).write_text(result)
+    
---
+++ SimpleSwitcher/src/flags/scripts/run.cmd
@@ -0,0 +1,1 @@
+npm run build-pngs -- 32:
