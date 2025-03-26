c1cb4e78047e3860f1b650fbde75e1fcd3919100
Alexsander <aegel5@yandex.ru>
2025-03-26 13:14:15 +0300

    add script

---
+++ SimpleSwitcher/src/flags/genres_resized.py
@@ -0,0 +1,18 @@
+﻿import pathlib
+from pathlib import Path
+import re
+import os
+import shutil 
+import sys
+
+result = ''
+
+for root, dirs, files in os.walk('./png16_resized'):
+    for file in files:
+        if file.endswith('.png'):    
+            result += '{}16 RCDATA "flags/png16_resized/{}"\n'.format(
+                Path(file).stem.upper(), os.path.basename(file)
+            )
+            
+Path("./flag_res16.rc").write_text(result)
+    
