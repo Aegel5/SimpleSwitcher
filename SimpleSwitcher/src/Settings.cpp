9b01665299604b22b4594e484d9b6171fcdc1004
Alexsander <aegel5@yandex.ru>
2025-03-31 23:50:02 +0300

    remove alt

--- SimpleSwitcher/src/Settings.cpp
+++ SimpleSwitcher/src/Settings.cpp
@@ -37,7 +37,7 @@ void SettingsGui::GenerateListHK()
 
     {
         CHotKeySet set;
-        set.def_list = { {VK_LMENU, VK_CAPITAL}, CHotKey(VK_LMENU, VK_PAUSE), CHotKey(VK_PAUSE), CHotKey(VK_LMENU,VK_F24) };
+        set.def_list = { CHotKey{VK_PAUSE} };
         set.fUseDef = true;
         set.gui_text = _(L"Change layout for selected text").wc_str();
         AddHotKey(hk_RevertSelelected, set);
@@ -62,6 +62,7 @@ void SettingsGui::GenerateListHK()
     {
         CHotKeySet set;
         set.def_list = { CHotKey(VK_SCROLL), CHotKey(VK_PAUSE), CHotKey(VK_F23) };
+        set.fUseDef = true;
         set.gui_text = _(L"Selected text to UPPER/lower case").wc_str();
         AddHotKey(hk_toUpperSelected, set);
     }
