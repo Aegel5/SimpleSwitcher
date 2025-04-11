ae7f09eab7d86ff712bb4115b7d1066956fd2917
Alexsander <aegel5@yandex.ru>
2025-04-11 15:38:21 +0300

    fix

--- SimpleSwitcher/localization/gen.cmd
+++ SimpleSwitcher/localization/gen.cmd
@@ -1,1 +1,1 @@
-"./gnuwin32/xgettext.exe" -d ss -s --keyword=_ -o mess.pot --from-code=utf-8  -C "../src/*.cpp" "../src/gui/*.cpp"
+"./gnuwin32/xgettext.exe" -d ss -s --keyword=_ -o mess.pot --from-code=utf-8  -C "../src/*.cpp" "../src/*.h" "../src/gui/*.h" "../src/gui/*.cpp"
