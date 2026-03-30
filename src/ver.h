36810006d5e178174c41c4cc60a1e28b0ff4ba64
Alexsander <aegel5@yandex.ru>
2026-03-30 15:38:58 +0300

    123

--- src/ver.h
+++ src/ver.h
@@ -6,6 +6,6 @@ inline const char* GET_SW_VERSION() {
 #ifdef PUBLIC_RELEASE
 	return details::SW_VERSION;
 #endif
-	static std::string ver{ details::SW_VERSION + "_U"s };
+	static std::string ver{ details::SW_VERSION + " PREVIEW"s };
 	return ver.c_str();
 }
