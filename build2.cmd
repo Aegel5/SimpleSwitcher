d5ba3246274f19192523ac813426decfa142c75e
Alexsander <aegel5@yandex.ru>
2026-03-30 16:04:43 +0300

    del old

--- build2.cmd
+++
@@ -1,1 +0,0 @@
-dotnet run --project SimpleSwitcherBuild/SimpleSwitcherBuild.csproj -c Release 
--- publish.py
+++
@@ -1,9 +0,0 @@
-﻿import subprocess
-import sys
-
-# Получаем все аргументы, кроме имени скрипта
-args = sys.argv[1:]
-args.append('/publish')
-
-# Запускаем второй скрипт с аргументами
-subprocess.run(['python', 'build.py'] + args)
