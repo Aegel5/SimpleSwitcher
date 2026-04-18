9a8c551ab453be4c3a284214fc32daf94294700c
Alexsander <aegel5@yandex.ru>
2026-04-18 13:56:43 +0300

    refact

--- src/libtools/utils/layout-utils.h
+++ src/libtools/utils/layout-utils.h
@@ -74,49 +74,49 @@ namespace Utils
 		PostMessage(hwnd, WM_INPUTLANGCHANGEREQUEST, 0, (LPARAM)lay);
 	}
 
-	inline TStatus SetLayByCom(HKL lay) {
+	//inline TStatus SetLayByCom(HKL lay) {
 
-		CComPtr<ITfInputProcessorProfileMgr> pProfile;
-		CComPtr<ITfInputProcessorProfiles> pProfile2;
+	//	CComPtr<ITfInputProcessorProfileMgr> pProfile;
+	//	CComPtr<ITfInputProcessorProfiles> pProfile2;
 
 
-		IFH_RET(CoCreateInstance(CLSID_TF_InputProcessorProfiles,
-			NULL,
-			CLSCTX_INPROC_SERVER,
-			IID_ITfInputProcessorProfileMgr,
-			(LPVOID*)&pProfile));
+	//	IFH_RET(CoCreateInstance(CLSID_TF_InputProcessorProfiles,
+	//		NULL,
+	//		CLSCTX_INPROC_SERVER,
+	//		IID_ITfInputProcessorProfileMgr,
+	//		(LPVOID*)&pProfile));
 
-		IFH_RET(CoCreateInstance(CLSID_TF_InputProcessorProfiles,
-			NULL,
-			CLSCTX_INPROC_SERVER,
-			IID_ITfInputProcessorProfiles,
-			(LPVOID*)&pProfile2));
+	//	IFH_RET(CoCreateInstance(CLSID_TF_InputProcessorProfiles,
+	//		NULL,
+	//		CLSCTX_INPROC_SERVER,
+	//		IID_ITfInputProcessorProfiles,
+	//		(LPVOID*)&pProfile2));
 
-		TF_INPUTPROCESSORPROFILE profile = {};
-		IFH_RET(pProfile->GetActiveProfile(GUID_TFCAT_TIP_KEYBOARD, &profile));
+	//	TF_INPUTPROCESSORPROFILE profile = {};
+	//	IFH_RET(pProfile->GetActiveProfile(GUID_TFCAT_TIP_KEYBOARD, &profile));
 
-		LANGID lang;
-		IFH_RET(pProfile2->GetCurrentLanguage(&lang));
+	//	LANGID lang;
+	//	IFH_RET(pProfile2->GetCurrentLanguage(&lang));
 
-		CComPtr<IEnumTfInputProcessorProfiles> profs_enum;
-		ULONG geted;
-		IFH_RET(pProfile->EnumProfiles(0, &profs_enum));
-		TF_INPUTPROCESSORPROFILE profs[100] = { 0 };
-		IFH_RET(profs_enum->Next(100, profs, &geted));
-		for (auto i = 0; i < geted; i++) {
-			auto& cur = profs[i];
-			if (cur.dwProfileType != TF_PROFILETYPE_KEYBOARDLAYOUT) continue;
-			if (cur.hkl == lay) {
+	//	CComPtr<IEnumTfInputProcessorProfiles> profs_enum;
+	//	ULONG geted;
+	//	IFH_RET(pProfile->EnumProfiles(0, &profs_enum));
+	//	TF_INPUTPROCESSORPROFILE profs[100] = { 0 };
+	//	IFH_RET(profs_enum->Next(100, profs, &geted));
+	//	for (auto i = 0; i < geted; i++) {
+	//		auto& cur = profs[i];
+	//		if (cur.dwProfileType != TF_PROFILETYPE_KEYBOARDLAYOUT) continue;
+	//		if (cur.hkl == lay) {
 
-				IFH_RET(pProfile->ActivateProfile(TF_PROFILETYPE_KEYBOARDLAYOUT, cur.langid, CLSID_NULL, GUID_NULL, cur.hkl, TF_IPPMF_ENABLEPROFILE | TF_IPPMF_FORSESSION));
-				//IFH_RET(pProfile2->ChangeCurrentLanguage(cur.langid));
-				LOG_ANY(L"switch ok to {}", (void*)lay);
-				RETURN_SUCCESS;
-			}
-		}
+	//			IFH_RET(pProfile->ActivateProfile(TF_PROFILETYPE_KEYBOARDLAYOUT, cur.langid, CLSID_NULL, GUID_NULL, cur.hkl, TF_IPPMF_ENABLEPROFILE | TF_IPPMF_FORSESSION));
+	//			//IFH_RET(pProfile2->ChangeCurrentLanguage(cur.langid));
+	//			LOG_ANY(L"switch ok to {}", (void*)lay);
+	//			RETURN_SUCCESS;
+	//		}
+	//	}
 
-		RETURN_SUCCESS;
-	}
+	//	RETURN_SUCCESS;
+	//}
 
 	inline TopWndInfo GetFocusedWndInfo() {
 
---
+++ src/old/utils/utils.h
@@ -0,0 +1,38 @@
+﻿//	inline bool ProcSingleton(const TChar* mtxName)
+//	{
+//#ifdef _WIN32
+//		HANDLE mtxGui = CreateMutex(NULL, FALSE, mtxName);
+//		if (GetLastError() == ERROR_ALREADY_EXISTS)
+//		{
+//			return true;
+//		}
+//		else if (GetLastError() != ERROR_SUCCESS)
+//		{
+//			IFW_LOG(FALSE);
+//		}
+//
+//		return false;
+//#else
+//		// Логика для Linux через абстрактные сокеты (аналог именованного мьютекса)
+//		int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+//		if (sock < 0) return false;
+//
+//		struct sockaddr_un addr;
+//		memset(&addr, 0, sizeof(addr));
+//		addr.sun_family = AF_UNIX;
+//
+//		// Первый байт 0 делает путь "абстрактным" — сокет исчезнет сам при закрытии процесса
+//		strncpy(addr.sun_path + 1, mtxName, sizeof(addr.sun_path) - 2);
+//
+//		if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+//			if (errno == EADDRINUSE) {
+//				close(sock);
+//				return true; // Уже запущено
+//			}
+//			close(sock);
+//			return false;
+//		}
+//		// Сокет намеренно не закрываем, чтобы держать "мьютекс"
+//		return false;
+//#endif
+//	}
